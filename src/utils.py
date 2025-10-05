3898c937f7b843df8a311c014ff3706148a2007a
Aston Crawley <astoncrawley@gmail.com>
2025-10-05 20:31:29 +0400

    Add loan repayment functions

--- src/utils.py
+++ src/utils.py
@@ -64,3 +64,97 @@ def calculate_interest(rpi, income, low_thresh=28470.00, high_thresh=51245.00):
         interest_rate = rpi + 3 * ((income - low_thresh) / (high_thresh - low_thresh))
     
     return interest_rate
+
+
+
+
+
+
+
+def calculatePlan2Repayments(salary, annual_thresh):
+    monthly_thresh = annual_thresh/12
+    charge = 0.09
+    
+    monthly_repayment = ((salary/12)-monthly_thresh)*charge
+    
+    if monthly_repayment < 0:
+        monthly_repayment = 0
+        
+    return monthly_repayment
+
+
+def calculatePostgradRepayments(salary):
+    month_thresh = 1750
+    charge = 0.06
+    
+    monthly_repayment = ((salary/12)-month_thresh)*charge
+    
+    if monthly_repayment < 0:
+        monthly_repayment = 0
+        
+    return monthly_repayment
+
+
+min_repayment = calculatePlan2Repayments(gross_salary, repayment_thresh)
+
+
+
+# def calculateAnnualPlan2Repayments(loan_remaining, min_repayment, interest_rate, extra_payment):
+#     new_loan_remaining = (loan_remaining-(min_repayment*12)-(extra_payment*12))
+#     if new_loan_remaining <= 0:
+#         return 0
+#     else:
+#         return new_loan_remaining
+
+
+def calculateAnnualPlan2Repayments(loan_remaining, min_repayment, interest_rate, extra_repayment):
+    total_minimum_repayments = 0
+    total_extra_repayments = 0
+
+    for month in range(12):
+        loan_remaining -= min_repayment
+        if loan_remaining <= 0:
+            total_minimum_repayments += abs(loan_remaining)
+            return 0, total_minimum_repayments, total_extra_repayments
+        else:
+            total_minimum_repayments += min_repayment
+    
+        loan_remaining -= extra_repayment
+        if loan_remaining <= 0:
+            total_extra_repayments += abs(loan_remaining)
+            return 0, total_minimum_repayments, total_extra_repayments
+        else:
+            total_extra_repayments += extra_repayment
+    
+    return loan_remaining, total_minimum_repayments, total_extra_repayments
+
+
+
+val = loan_remaining
+val_an = []
+
+for i in range(30):
+    val = (val-(min_repayment*12))*study_interest
+    val_an.append(val)
+    
+plt.plot(val_an)
+plt.show()
+
+print("Total amount paid over 30 years")
+print(min_repayment*12*30)
+
+
+
+
+
+def calculateInterest(initial_pot, monthly_addition, interest_rate, years):
+    
+    annual_pot = [0]
+    
+    for i in range(years):
+        end_pot = initial_pot + (monthly_addition*12)
+        annual_average = initial_pot + (end_pot - initial_pot)
+        initial_pot = annual_average * interest_rate
+        annual_pot.append(initial_pot)
+    
+    return annual_pot
